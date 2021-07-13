8bbe31e1cbde72b6eaa20942a04eb0938ec661d6
Lewis Shaw <45900375+purplefrizzel@users.noreply.github.com>
2021-07-14 00:08:10 +0100

    Created a testing framework for unit tests and also added CPU exception capturing.
    * Testing framework
    * CPU exception capturing
    * Created Libnoodleos

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
 #![feature(custom_test_frameworks)]
 #![test_runner(noodleos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -10,26 +9,17 @@ use noodleos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    noodleos::init();
-
-    println!("Hello world!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     noodleos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
